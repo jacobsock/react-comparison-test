63f1da731c189b17c715863aa442d4cda7bf056d
Jacob Sock <jacobmsock@gmail.com>
2024-10-15 17:44:32 -0400

    Initial Commit

---
+++ react-comparison-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  react-comparison-test
+//
+//  Created by Jacob M Sock  on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ react-comparison-test/react_comparison_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  react_comparison_testApp.swift
+//  react-comparison-test
+//
+//  Created by Jacob M Sock  on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct react_comparison_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
