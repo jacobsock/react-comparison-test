538d7c15b38cb4b5b771bca81a6539760728f0d9
Jacob Sock <jacobmsock@gmail.com>
2024-10-15 17:51:03 -0400

    add buttons illustrating different basic state options in swift, comparing roughly to React

--- react-comparison-test/ContentView.swift
+++ react-comparison-test/ContentView.swift
@@ -8,16 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var elevatedStateButton1Count = 0
+    @State var elevatedStateButton2Count = 0
+    @State var elevatedSharedStateButtonCount = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Counters that illustrating state patterns:")
+                .font(.title)
+                .padding()
+            
+            SelfContainedStateButton1()
+            SelfContainedStateButton2()
+            ElevatedStateButton1(count: $elevatedStateButton1Count)
+            ElevatedStateButton2(count: $elevatedStateButton2Count)
+            HStack{
+                ElevatedSharedStateButton(count: $elevatedSharedStateButtonCount)
+                ElevatedSharedStateButton(count: $elevatedSharedStateButtonCount)
+            }
+
         }
-        .padding()
     }
 }
+
+///Example of how state can be contained within the button as in react
+struct SelfContainedStateButton1: View {
+    @State private var count = 0
+
+    var body: some View {
+        Button(action: {
+            count += 1
+        }) {
+            Text("Clicked \(count) times")
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+    }
+}
+
+///Example of how state can be contained within the button as in react
+struct SelfContainedStateButton2: View {
+    @State private var count = 0
+
+    var body: some View {
+        Button(action: {
+            count += 10
+        }) {
+            Text("Clicked \(count) times")
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+    }
+}
+///Example of how state can be elevated to parent as expected in SwiftUI
+struct ElevatedStateButton1: View {
+    @Binding  var count : Int
+
+    var body: some View {
+        Button(action: {
+            count += 1
+        }) {
+            Text("Clicked \(count) times")
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+    }
+}
+///Example of how state can be elevated to parent as expected in SwiftUI
+struct ElevatedStateButton2: View {
+    @Binding  var count : Int
+
+    var body: some View {
+        Button(action: {
+            count += 10
+        }) {
+            Text("Clicked \(count) times")
+                .padding()
+                .background(Color.orange)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+    }
+}
+
+//Example of how state can be elevated cause saming changes among subviews, similar to React.
+struct ElevatedSharedStateButton: View {
+    @Binding  var count : Int
+
+    var body: some View {
+        Button(action: {
+            count += 5
+        }) {
+            Text("Clicked \(count) times")
+                .padding()
+                .background(Color.black)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+    }
+}
+
+
+
 
 #Preview {
     ContentView()
